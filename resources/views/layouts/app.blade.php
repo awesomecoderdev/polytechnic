3ffd2405f2876c6f9ae20fd70298acc3375aed00
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-10-15 17:42:40 +0600

    Updated UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,36 +5,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    {{-- start::head --}} @yield('head') {{-- end::head --}}
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+<body class=" min-h-screen bg-background font-sans antialiased ">
+    {{-- start::header --}} @yield('header') {{-- end::header --}}
 
-        <!-- Page Heading -->
-        @isset($header)
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+    <main id="main"
+        class="relative min-h-[calc(100vh-75px)] lg:px-8 sm:px-7 xs:px-5 px-4 xl:overflow-visible overflow-hidden">
+        @isset($slot)
+            {{ $slot }}
         @endisset
-
-        <!-- Page Content -->
-        <main>
-            @isset($slot)
-                {{ $slot }}
-            @endisset
-        </main>
-    </div>
+    </main>
+    {{-- start::content --}} @yield('content') {{-- end::content --}}
+    {{-- start::script --}} @yield('script') {{-- end::script --}}
 </body>
 
 </html>
